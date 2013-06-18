2726832e78d878529848a06290a14839630b4d1e
huyettcp <huyettcp@gmail.com>
2013-06-18 12:17:20 -0400

    Madlibs

---
+++ madlibs.rb
@@ -0,0 +1,22 @@
+puts "Give me a city in the United States"
+city = gets.chomp
+
+puts "adjective"
+first_adj = gets.chomp
+
+puts "another adjective"
+second_adj = gets.chomp
+
+puts "mode of transportation"
+transportation = gets.chomp
+
+puts "speed of travel"
+speed = gets.chomp
+
+puts "architecture style"
+arch = gets.chomp
+
+puts "adjective"
+third_adj = gets.chomp
+
+puts "The city of #{city} in the United States, which I am currently visiting, is extemely #{first_adj} and #{second_adj}. The citizens of #{city} generally travel by #{transportation}, which is a very #{speed} way of getting around. The buildings all look like they are influenced by #{arch} style, which gives the city a very #{third_adj} vibe."
